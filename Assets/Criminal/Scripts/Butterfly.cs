3b7418397d551168eb40eeb3dda1a87e3559f205
Fang <74826581+mn1shi@users.noreply.github.com>
2024-01-05 13:50:54 +0000

    touch interaction test

--- Assets/Criminal/Scripts/Butterfly.cs
+++ Assets/Criminal/Scripts/Butterfly.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Butterfly : MonoBehaviour
 {
 
-    public GameObject UITest;
-    public GameObject froggerVid;
+    public GameObject headC;
+    public GameObject headL;
+    public GameObject headR;
 
     // Update is called once per frame
     void Update()
@@ -22,24 +23,24 @@ public class Butterfly : MonoBehaviour
                 Debug.Log("hit");
                 Debug.Log(hit.transform.name + " : " + hit.transform.tag);
 
-                if (hit.transform.tag == "butterfly")
+                if (hit.transform.tag == "headC")
                 {
-                    //Vector3 pos = hit.point;
-                    //pos.z += 0.25f;
-                    //pos.y += 0.25f;
-                    //Instantiate(UITest, pos, transform.rotation);
-                    Animator anim = hit.transform.GetComponent<Animator>();
+                    Vector3 pos = hit.point;
+                    pos.z += 0.25f;
+                    pos.y += 0.25f;
+                    Instantiate(headC, pos, transform.rotation);
+                    //Animator anim = hit.transform.GetComponent<Animator>();
                 }
 
-                //if (hit.transform.tag == "Info")
-                //{
-                //    Destroy(hit.transform.gameObject);
-                //}
+                if (hit.transform.tag == "headL")
+                {
+                    Destroy(hit.transform.gameObject);
+                }
 
-                //if (hit.transform.tag == "FroggerVid")
-                //{
-                //    Destroy(hit.transform.gameObject);
-                //}
+                if (hit.transform.tag == "headR")
+                {
+                    Destroy(hit.transform.gameObject);
+                }
 
             }
         }
